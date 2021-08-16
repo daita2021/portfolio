99db17267196c2dc4ef74774dd16bd8655a7f12d
daita2021 <daitamura2021@gmail.com>
2021-08-16 12:27:22 +0900

    contact form実装

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,7 +1,27 @@
+import "./About.css";
+
 export const About = () => {
   return (
     <>
       <h2>ABOUT</h2>
+      <div className="commonBox">a</div>
+      <div className="commonBox formWrapper">
+        <h3 className="contact">Contact</h3>
+        <div className="formBox">
+          <form name="contact" action="/" method="post" netlify="true">
+            <input type="text" name="name" required autoComplete="name" placeholder="Name" />
+
+            <input type="email" name="email" required autoComplete="email" placeholder="E-mail" />
+
+            <textarea name="message" placeholder="Comment" required></textarea>
+
+            <p className="formBtn">
+              <input type="submit" value="SUBMIT" />
+              <input type="reset" value="RESET" />
+            </p>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
