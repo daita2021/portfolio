c898a61266f7bf4d94375521584d722e33d83bc0
daita2021 <daitamura2021@gmail.com>
2021-09-11 23:59:14 +0900

    js記事追加

--- src/containers/Js.jsx
+++ src/containers/Js.jsx
@@ -7,6 +7,7 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 import TestImg from "../images/test.jpg";
 import Weather from "../images/weather.jpg";
 import NumberGuees from "../images/number.jpg";
+import Clock from "../images/clock.jpg";
 
 export const Js = () => {
   const [isloading, setLoading] = useState(true);
@@ -32,7 +33,7 @@ export const Js = () => {
       <div className="commonBox worksFlex">
         <Article title="天気 API" imgUrl={Weather} siteUrl="http://portfoliotam.starfree.jp/weather-api/" description="Weather APIを利用した天気情報表示ツールです"></Article>
         <Article title="数字当てゲーム" imgUrl={NumberGuees} siteUrl="http://portfoliotam.starfree.jp/number-guessing/number-guessing-game.html" description="1~100までの数字を10ターン以内に当てるゲームです"></Article>
-        <Article title="test2" imgUrl={TestImg}></Article>
+        <Article title="デジタル時計" imgUrl={Clock} siteUrl="https://jolly-carson-b80976.netlify.app/" description="デジタル表示の時計です"></Article>
         <Article title="test3" imgUrl={TestImg}></Article>
       </div>
     </>
