801db4a69602e1ae486174de7fb47b63c5ab7b08
daita2021 <daitamura2021@gmail.com>
2021-09-14 09:43:23 +0900

    jsに記事追加

--- src/containers/Js.jsx
+++ src/containers/Js.jsx
@@ -8,6 +8,7 @@ import TestImg from "../images/test.jpg";
 import Weather from "../images/weather.jpg";
 import NumberGuees from "../images/number.jpg";
 import Clock from "../images/clock.jpg";
+import TaskList from "../images/tasklist.jpg";
 
 export const Js = () => {
   const [isloading, setLoading] = useState(true);
@@ -32,9 +33,10 @@ export const Js = () => {
       )}
       <div className="commonBox worksFlex">
         <Article title="天気 API" imgUrl={Weather} siteUrl="http://portfoliotam.starfree.jp/weather-api/" description="Weather APIを利用した天気情報表示ツールです"></Article>
+        <Article title="ToDoリスト" imgUrl={TaskList} siteUrl="https://focused-wozniak-b77c3d.netlify.app/" description="シンプルなtodoリストです。データ保持はローカルストレージを使用しています。"></Article>
         <Article title="数字当てゲーム" imgUrl={NumberGuees} siteUrl="http://portfoliotam.starfree.jp/number-guessing/number-guessing-game.html" description="1~100までの数字を10ターン以内に当てるゲームです"></Article>
         <Article title="デジタル時計" imgUrl={Clock} siteUrl="https://jolly-carson-b80976.netlify.app/" description="デジタル表示の時計です"></Article>
-        <Article title="test3" imgUrl={TestImg}></Article>
+        <Article title="test" imgUrl={TestImg}></Article>
       </div>
     </>
   );
